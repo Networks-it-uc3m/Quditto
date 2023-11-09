7dd0b40a85f7bd364b79af9a1e81f42f15e437e3
Borjand <bdorado@pa.uc3m.es>
2023-11-09 14:57:38 +0000

    Clean old version of the  qdts_orchestrator repository

--- compile.sh
+++ compile.sh
@@ -2,7 +2,8 @@
 
 python -m grpc_tools.protoc --proto_path=./protobufs --python_out=./qdts_node/src/qdts_node/grpc_serialization --grpc_python_out=./qdts_node/src/qdts_node/grpc_serialization ./protobufs/*.proto
 cp ./qdts_node/src/qdts_node/grpc_serialization/* ./client_lib/
-cd qdts_node 
+cd qdts_node
 poetry build
-cd ../qdts_orquestrator
-poetry build
+#cd ../qdts_orquestrator
+#poetry build
+
