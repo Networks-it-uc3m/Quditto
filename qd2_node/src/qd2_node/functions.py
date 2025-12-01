7a917d99bc8041f4349f99ea5a6d22c7ad617fe9
jpmon201 <126093994+jpmon201@users.noreply.github.com>
2025-12-01 11:36:58 +0100

    Update functions.py

--- qd2_node/src/qd2_node/functions.py
+++ qd2_node/src/qd2_node/functions.py
@@ -2,8 +2,10 @@ import yaml
 import pika
 import json
 import secrets
+import subprocess
 
-def get_key(origin=None, node=None, key=None):
+
+def get_key_QKD(origin=None, node=None, key=None):
         
     call_id = secrets.token_hex(16)
     
@@ -29,6 +31,7 @@ def get_key(origin=None, node=None, key=None):
     message = {"origin":str(origin), "node": str(node), "key": str(key), "call_id": call_id}
     json_m = json.dumps(message)
 
+
     channel.basic_publish(exchange='direct_logs', routing_key="api"+origin, body=json_m)
 
     queue_name = result.method.queue
@@ -37,7 +40,8 @@ def get_key(origin=None, node=None, key=None):
     # Wait for a message from the queue with key "A"
     def on_message(ch, method, properties, body):
         nonlocal text
-        text = json.loads(body)
+        response = json.loads(body)
+        text["keys"] = [response]
         ch.basic_ack(delivery_tag=method.delivery_tag)
         connection.close()  # Close connection once message is received
 
@@ -49,7 +53,9 @@ def get_key(origin=None, node=None, key=None):
 
     return text
 
-def get_key_with_ID(origin=None, node=None, key_ID=None):
+
+
+def get_key_with_ID_QKD(origin=None, node=None, key_ID=None):
 
     call_id = secrets.token_hex(16)
     
@@ -75,6 +81,7 @@ def get_key_with_ID(origin=None, node=None, key_ID=None):
     message = {"origin":str(origin), "node": str(node), "key_ID": str(key_ID), "call_id": call_id}
     json_m = json.dumps(message)
 
+
     channel.basic_publish(exchange='direct_logs', routing_key="api"+origin, body=json_m)
 
     queue_name = result.method.queue
@@ -93,3 +100,79 @@ def get_key_with_ID(origin=None, node=None, key_ID=None):
     channel.start_consuming()
 
     return text
+
+
+def get_key_PQC(node_ip=None):
+
+    # Parameter validation
+    if node_ip is None:
+        return {"error": "Server's IP not specified"}
+
+    command = ['python3', 'client.py', '-ip', str(node_ip)]
+
+    result = subprocess.run(command, capture_output=True, text=True)
+
+    # Check if there was an error during script execution
+    if result.returncode != 0:
+        return {"error": "client.py execution failed", "stderr": result.stderr}
+
+    try:
+        output = json.loads(result.stdout)
+
+        key = output["Secret_key"]
+        key_ID = output["ID"]
+
+        return {
+            "keys": [
+                {
+                    "key_ID": key_ID,
+                    "key": key
+                }
+            ]
+        }
+
+    except (json.JSONDecodeError, KeyError) as e:
+        return {
+            "error": f"Error processing the results: {e}",
+            "stdout": result.stdout
+        }
+
+
+def get_key_with_ID_PQC(node_ip, key_ID):
+
+    # Parameter validation
+    if node_ip is None:
+        return {"error": "Server's IP not specified"}
+
+    command = ['python3', 'client.py', '-ip', str(node_ip), '-id', str(key_ID)]
+
+    # Execute the command
+    result = subprocess.run(command, capture_output=True, text=True)
+
+    # Check if there was an error during script execution
+    if result.returncode != 0:
+        return {
+            "error": "client.py execution failed",
+            "stderr": result.stderr
+        }
+
+    try:
+        output = json.loads(result.stdout)
+
+        key = output["Secret_key"]
+        key_ID = output["ID"]
+
+        return {
+            "keys": [
+                {
+                    "key_ID": key_ID,
+                    "key": key
+                }
+            ]
+        }
+
+    except (json.JSONDecodeError, KeyError) as e:
+        return {
+            "error": f"Error processing results: {e}",
+            "stdout": result.stdout
+        }
